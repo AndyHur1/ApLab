81539d86a0b6baa8db4f11f0fa0fde90337a6e2f
AndyHur1 <hurby@ssfs.org>
2020-02-19 14:43:46 -0500

    dzxd

---
+++ src/ReviewDriver.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class ReviewDriver {
+    public static double sentimentVal (String word){
+        return 0;
+    }
+    public static void main(String[]args){
+        System.out.println(Review.sentimentVal("among"));
+        System.out.println(Review.sentimentVal("math"));
+        System.out.println(Review.sentimentVal("smile"));
+        double num =sentimentVal("warm");
+
+    }
+}
